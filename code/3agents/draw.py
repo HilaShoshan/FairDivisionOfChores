72e8cf8f513ebd226822f126ad25b22cbc235a9c
Hila Shoshan <hilashoshan0605@gmail.com>
2022-03-03 13:56:56 +0200

    add background for each allocation

--- code/3agents/draw.py
+++ code/3agents/draw.py
@@ -1,15 +1,18 @@
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import matplotlib.pyplot as plt
 import numpy as np
+import warnings
+warnings.simplefilter("ignore", UserWarning)
 
 
 def draw_3D_triangle():
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
-    poly3d = np.array([[1, 0, 0],
-                       [0, 1, 0],
-                       [0, 0, 1]])
+    triangle = np.array([[1, 0, 0],
+                         [0, 1, 0],
+                         [0, 0, 1]])
 
-    ax.add_collection3d(Poly3DCollection(poly3d, facecolors="white", linewidths=1, alpha=0.8))
+    ax.add_collection3d(Poly3DCollection(triangle, facecolors="white", linewidths=1, alpha=0.4))
+
     return fig, ax
--- code/3agents/moving_knife.py
+++ code/3agents/moving_knife.py
@@ -8,7 +8,7 @@ from collections import deque
 from functions import *
 from draw import *
 from numpy.random import randint
-import numpy as np
+import random
 
 print("Division A = (A1,A2,A3)\n")
 
@@ -43,12 +43,12 @@ def get_category(m):
     )
 
 
-# utilities = [get_category(3), get_category(3), get_category(3)]
-utilities = [
-    [(-86, -1, -71), (-179, -139, -162), (-287, -256, -210)],
-    [(-78, -84, -89), (-154, -163, -172), (-236, -253, -206)],
-    [(-33, -85, -51), (-152, -110, -196), (-207, -213, -203)],
-]
+utilities = [get_category(3), get_category(3), get_category(3)]
+# utilities = [
+#     [(-86, -1, -71), (-179, -139, -162), (-287, -256, -210)],
+#     [(-78, -84, -89), (-154, -163, -172), (-236, -253, -206)],
+#     [(-33, -85, -51), (-152, -110, -196), (-207, -213, -203)],
+# ]
 print("utilities: ", utilities)
 capacities = (1, 1, 1)
 
@@ -63,32 +63,49 @@ colors_dict = {1: "red", 2: "orange", 3: "blue"}
 allocations_dict = {}  # a dictionary that saves the allocations as keys, with a list of points as values
 
 done = False
+counter = 0
 for w3 in np.arange(0, 1+epsilon, step):
     for w2 in np.arange(0, 1-w3+epsilon, step):
         w1 = 1 - w2 - w3
         # print("point: ", (w1, w2, w3))
+        counter += 1  # count the number of points we have
         G = create_G(utilities, capacities, (w1, w2, w3))
         matching = nx.max_weight_matching(G, maxcardinality=True)
         A1, A2, A3 = get_allocations(matching)
         key = ' '.join(map(str, [A1, A2, A3]))
-        # print(key)
         if key in allocations_dict:
             new_point = (w1, w2, w3)
             res = deque(allocations_dict[key])
             res.appendleft(new_point)
             new_list = list(res)  # add this point to the dict
+            allocations_dict[key] = new_list
         else:
             allocations_dict[key] = [(w1, w2, w3)]  # create a new entry with the new allocation
         # print("A1: ", A1)
         # print("A2: ", A2)
         # print("A3: ", A3)
         if isEF1(A1, A2, A3, utilities, ax, w1, w2, w3, colors_dict):
-            print("done")
+            # print("done")
             done = True
             # break
         # print("_______________________________________")
     # if done:
       #  break
 
-# print("****************", allocations_dict)
+
+def generate_random_color():
+    return ["#"+''.join([random.choice('ABCDEF0123456789') for i in range(6)])]
+
+
+lst = []
+for key in allocations_dict.keys():
+    value = allocations_dict[key]  # a list of points (tuples)
+    lst.append([key, len(value)])
+    background = [value]  # a list of the polygon points
+    facecolor = generate_random_color()
+    ax.add_collection3d(Poly3DCollection(background, facecolors=facecolor, linewidths=1, alpha=1))
+
+print(tabulate(lst, headers=['allocation', 'num of points']))
+print("num of points: ", counter)
+
 plt.show()
